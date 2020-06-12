740f6b2b085987c1063a451fdbced7947f9cea8a
anshikaj2414 <41519947+anshikaj2414@users.noreply.github.com>
2020-06-12 18:51:54 +0530

    Create InsertNewNodeAtHead.c

---
+++ InsertNewNodeAtHead.c
@@ -0,0 +1,7 @@
+SinglyLinkedListNode* insertNodeAtHead(SinglyLinkedListNode* llist, int data) {
+
+SinglyLinkedListNode* temp = (SinglyLinkedListNode*)malloc(sizeof(SinglyLinkedListNode)); temp->data = data; 
+temp->next = llist;
+ return temp;
+
+}
